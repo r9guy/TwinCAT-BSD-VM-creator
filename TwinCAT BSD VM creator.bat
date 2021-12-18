ec612d580f27c6b565a784026b54ca390872569a
r9guy <9428627+r9guy@users.noreply.github.com>
2021-12-18 18:14:18 +0400

    initial commit

---
+++ TwinCAT BSD VM creator.bat
@@ -0,0 +1,34 @@
+SET current_directory=%~dp0
+ECHO %current_directory%
+
+cd "/Program Files/Oracle/VirtualBox
+echo "creating Virtual Machine TwinCAT 3 BSD"
+
+SET vmname="TwinCAT 3 BSD"
+VBoxManage createvm --name %vmname% --basefolder %current_directory% --ostype FreeBSD_64 --register
+VBoxManage modifyvm %vmname% --memory 1024 --vram 128 --acpi on --hpet on --graphicscontroller vmsvga --firmware efi64
+
+set installer_image=TcBSD_installer.vdi
+set runtime_image=TcBSD.vhd
+set vmdirectory=%current_directory%\%vmname%
+set sink=%vmdirectory%\%installer_image%
+set source=%current_directory%TCBSD-x64-12-40411.iso
+
+echo "converting img image to virtualbox bootable HDD image"
+VBoxManage convertfromraw --format VDI %source% %sink%
+ 
+echo "Creating SATA storage Controller"
+VBoxManage storagectl %vmname% --name SATA --add sata --controller IntelAhci --hostiocache on --bootable on
+
+echo "attaching to installation HDD to Sata Port 1
+VBoxManage storageattach %vmname% --storagectl "SATA" --device 0 --port 1 --type  hdd --medium %sink%
+
+echo "creating empty HDD"
+set runtime_hdd=%vmdirectory%\%runtime_image%
+VBoxManage createmedium --filename %runtime_hdd% --size 4096 --format VHD
+
+echo "attaching created HDD to Sata Port 0 where we will install TwinCAT BSD"
+VBoxManage storageattach %vmname% --storagectl "SATA" --device 0 --port 0 --type  hdd --medium %runtime_hdd%
+
+
+pause
