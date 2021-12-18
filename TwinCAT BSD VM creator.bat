1f33cf65807308b35aa538dd3de5b8480961905d
r9guy <9428627+r9guy@users.noreply.github.com>
2021-12-18 18:35:11 +0400

    Add files via upload

--- TwinCAT BSD VM creator.bat
+++ TwinCAT BSD VM creator.bat
@@ -1,5 +1,5 @@
 SET current_directory=%~dp0
-ECHO %current_directory%
+SET sourcefilename="TCBSD-x64-12-40411.iso"
 
 cd "/Program Files/Oracle/VirtualBox
 echo "creating Virtual Machine TwinCAT 3 BSD"
@@ -12,11 +12,11 @@ set installer_image=TcBSD_installer.vdi
 set runtime_image=TcBSD.vhd
 set vmdirectory=%current_directory%\%vmname%
 set sink=%vmdirectory%\%installer_image%
-set source=%current_directory%TCBSD-x64-12-40411.iso
+set source=%current_directory%\%sourcefilename%
 
 echo "converting img image to virtualbox bootable HDD image"
 VBoxManage convertfromraw --format VDI %source% %sink%
- 
+
 echo "Creating SATA storage Controller"
 VBoxManage storagectl %vmname% --name SATA --add sata --controller IntelAhci --hostiocache on --bootable on
 
@@ -29,6 +29,3 @@ VBoxManage createmedium --filename %runtime_hdd% --size 4096 --format VHD
 
 echo "attaching created HDD to Sata Port 0 where we will install TwinCAT BSD"
 VBoxManage storageattach %vmname% --storagectl "SATA" --device 0 --port 0 --type  hdd --medium %runtime_hdd%
-
-
-pause
