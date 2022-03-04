8945a490c69bfa45e143f92a6fe7822dee2d7e67
r9guy <9428627+r9guy@users.noreply.github.com>
2022-03-05 03:13:25 +0400

    new features
    Now VM creates 2 network interfaces,
    1st will be associated with Host only interface if it exists, otherwise will create one(currently not working due to bug in VirtualBox)
    2nd NAT interface to give OS ability to access internet.

--- TwinCAT BSD VM creator.bat
+++ TwinCAT BSD VM creator.bat
@@ -1,5 +1,3 @@
-cls
-echo off
 SET current_directory=%~dp0
 SET sourcefilename="TCBSD-x64-12-44089.iso"
 
@@ -14,16 +12,28 @@ set vmdirectory=%current_directory%%vmname:"=%
 set sink=%vmdirectory%\%installer_image:"=%
 set source=%current_directory%%sourcefilename:"=%
 
-echo "before conversion verify that the path exists, or create the directory"
-
-if not exist "%vmdirectory%"\ mkdir "%vmdirectory%"
 
 echo "converting img image to virtualbox bootable HDD image"
 VBoxManage convertfromraw "%source%" "%sink%" --format VHD
 
+:recreatevm
 echo "creating Virtual Machine TwinCAT 3 BSD"
 VBoxManage createvm --name %vmname% --basefolder %current_directory% --ostype FreeBSD_64 --register
+if  ERRORLEVEL 1    (
+    echo VM already exists, deleteing existing
+    VBoxManage unregistervm %vmname% --delete 
+    goto recreatevm
+) 
+
 VBoxManage modifyvm %vmname% --memory 2048 --vram 128 --acpi on --hpet on --graphicscontroller vmsvga --firmware efi64
+:recreateIf
+VBoxManage modifyvm %vmname% --nic1 hostonly --hostonlyadapter1 vboxnet1
+if ERRORLEVEL 1 (
+    echo host interface doesnt exist, creating one
+    VBoxManage hostonlyif create
+    goto recreateIf
+)
+VBoxManage modifyvm %vmname% --nic2 nat
 
 
 echo "Creating SATA storage Controller"
@@ -33,7 +43,7 @@ VBoxManage storagectl %vmname% --name SATA --add sata --controller IntelAhci --h
 echo "attaching to installation HDD to Sata Port 1
 VBoxManage storageattach %vmname% --storagectl "SATA" --device 0 --port 1 --type  hdd --medium "%sink%"
 
-set /p hddsize="Storege sise in MB:"
+set /p hddsize="Storege size in MB:"
 
 echo "creating empty HDD"
 set runtime_hdd=%vmdirectory%\%runtime_image:"=%
@@ -42,4 +52,4 @@ VBoxManage createmedium --filename "%runtime_hdd%" --size %hddsize% --format VHD
 
 echo "attaching created HDD to Sata Port 0 where we will install TwinCAT BSD"
 VBoxManage storageattach %vmname% --storagectl "SATA" --device 0 --port 0 --type  hdd --medium "%runtime_hdd%"
-pause
+
